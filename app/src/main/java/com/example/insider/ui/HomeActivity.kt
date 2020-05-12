1801ca40060c76602a63a1009ed404e344cbbb2e
Pranit Rane <rane.pranit.b@gmail.com>
2020-05-12 15:41:31 +0530

    Initial commit

---
+++ app/src/main/java/com/example/insider/ui/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.example.insider.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.insider.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}

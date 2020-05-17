f561c3dae3cc2a644bb57dc1f7be5e215bcc6e33
Pranit Rane <rane.pranit.b@gmail.com>
2020-05-17 20:16:33 +0530

    update: visuals[II] 1. animations

--- app/src/main/java/com/example/insider/ui/group/GroupFragment.kt
+++ app/src/main/java/com/example/insider/ui/group/GroupFragment.kt
@@ -2,7 +2,6 @@ package com.example.insider.ui.group
 
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
